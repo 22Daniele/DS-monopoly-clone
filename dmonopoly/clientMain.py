b6ee64e673aa60e059097946836cd76f72065345
daniele <daniele.loverre@gmail.com>
2026-07-03 19:14:57 +0200

    fix clientMain.py

--- dmonopoly/clientMain.py
+++ dmonopoly/clientMain.py
@@ -2,12 +2,11 @@ import sys
 import pygame
 from remote.bank_user import MonopolyUser
 
-if __name__ == "__main__":
-    # 1. Controllo degli argomenti da terminale
+
+def main():
     if len(sys.argv) < 4:
         print("ERRORE: Parametri mancanti.")
-        print("Uso corretto: python client_main.py <IP_SERVER> <PORTA> <NICKNAME>")
-        print("Esempio: python client_main.py 192.168.1.5 5000 Daniele")
+        print("Uso corretto: monopoly-client <IP_SERVER> <PORTA> <NICKNAME>")
         sys.exit(1)
 
     host = sys.argv[1]
@@ -20,20 +19,19 @@ if __name__ == "__main__":
 
     nickname = sys.argv[3]
 
-    # 2. Inizializzazione del motore grafico
     pygame.init()
-
     print(f"Avvio di MonopolyClient...")
-    print(f"Giocatore: {nickname}")
     print(f"Tentativo di connessione a: {host}:{port}...")
 
-    # 3. Creazione e avvio del Client
     try:
         client_app = MonopolyUser(nickname, (host, port))
         client_app.run()
     except Exception as e:
         print(f"Errore fatale del client: {e}")
     finally:
-        # Assicura una chiusura pulita se qualcosa va storto fuori dal loop
         pygame.quit()
         sys.exit(0)
+
+
+if __name__ == "__main__":
+    main()
